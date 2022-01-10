ebfa80a2d38b1f4c0d83f2d073678d390ac1a2b2
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-01-10 14:04:55 +0200

    Deploying

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,23 @@
+from app import db, app
+from os.path import join, dirname, realpath
+import os
+from flask_login import  login_required, LoginManager, UserMixin, login_manager, login_user, current_user, logout_user
+from flask import Flask, request, session, render_template, url_for, flash, get_flashed_messages, message_flashed
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import PY3, Bcrypt
+from sqlalchemy.orm import relationship
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, validators
+from werkzeug.utils import redirect, secure_filename
+from wtforms.fields.numeric import IntegerField
+from wtforms.fields.simple import EmailField, SubmitField, FileField
+
 from flask import Flask, abort
 from flask import render_template, request
 from flask.helpers import flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_manager, LoginManager, current_user, login_required, login_user, logout_user, UserMixin, user_logged_in
 from werkzeug.utils import redirect
-
-# app creation adn configuration
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Lethabo2016.@localhost:3306/app'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know"
-db = SQLAlchemy(app)
-
-
-# handling logins
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
@@ -21,7 +25,6 @@ login_manager.login_view = 'login'
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
     
 # classes to be used
 class User(db.Model, UserMixin):
@@ -263,13 +266,7 @@ def page_not_found(e):
     return render_template('history.html'), 400
 
 
-def GetApp():
-    return app
 
-if __name__ =="__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
 
 
 
